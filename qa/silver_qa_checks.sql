14b8e7e02dca9bd8cfab86098d00ab7b38b31823
Roxani Kyritsi <roxannekyr@users.noreply.github.com>
2026-05-08 22:14:25 +0300

    Update silver_qa_checks.sql

--- qa/silver_qa_checks.sql
+++ qa/silver_qa_checks.sql
@@ -10,10 +10,6 @@ Script Purpose:
     - Data standardization and consistency.
     - Invalid date ranges and orders.
     - Data consistency between related fields.
-
-Usage Notes:
-    - Run these checks after data loading Silver Layer.
-    - Investigate and resolve any discrepancies found during the checks.
 ===============================================================================
 */
 
